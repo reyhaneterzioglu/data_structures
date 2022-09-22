4a68400de285ba2eabaa3429761de473559e19b7
reyhaneterzioglu <terzioglureyhane@gmail.com>
2022-09-22 16:37:43 +0200

    Arrays

---
+++ dsa-02-Arrays/src/main/java/ArrayDemo.java
@@ -0,0 +1,10 @@
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+
+        int[] scoreArray = new int[10];
+
+        System.out.println(scoreArray); // [I@7dc36524 - beginning address, reference
+
+    }
+}
